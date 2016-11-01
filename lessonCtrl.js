7b1ae6b4f8086c4eff6ea02399c45d134bd2139c
olsondeven <olsondeven@gmail.com>
2016-11-01 17:44:21 -0600

    finished

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,3 +1,6 @@
 angular.module('directivePractice').controller('lessonCtrl', function($scope,lessonSrvc){//What are the options () for arguments
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+  $scope.announceDay = function(lesson, day){
+    alert(lesson + ' is active on ' + day + '.');
+  };
 });
--- lessonHider.js
+++ lessonHider.js
@@ -4,19 +4,21 @@ angular.module('directivePractice').directive('lessonHider', function() {
         templateUrl: 'lessonHider.html',
         scope: { // is a wall that helps you controller what is seen from controller $scope, = will allow you to make it same as Ctrl $scope
             // twoWayDataBinding: '=',//constant communication, or to limit scope
-            // stringBinding: '@',//just for show ?
-            // functionBinding: '&',//just for show ?
-            lesson: '='
+            // stringBinding: '@',//just for show ? pass text
+            // functionBinding: '&',//just for show ? pass a function
+            lesson: '=',
+            dayAlert: '&'
         },
         link: function(scope, element, attrs) {
             // console.log(scope, element, attrs);
+
             scope.getSchedule.then(function(response) {
                 scope.schedule = response.data;
-
+              
                 scope.schedule.forEach(function(scheduleDay) {
-                  console.log(scheduleDay);
                     if (scheduleDay.lesson === scope.lesson) {
                         element.css('text-decoration', 'line-through');// jquery, get to know this better
+                        scope.lessonDay = scheduleDay.weekday;
                         return;
                     }
                 });
